5743ef90e8221cd87fce1d57fde576afbc9fd8cd
andreiyv <voran@inbox.ru>
2015-08-19 15:31:20 +0300

    STOP_FOTOBOT implemented

--- app/src/main/java/com/example/andrey/fotobot/MainActivity.java
+++ app/src/main/java/com/example/andrey/fotobot/MainActivity.java
@@ -17,32 +17,26 @@ import java.io.OutputStreamWriter;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-
+int n;
     Handler hndl;
 
     Handler.Callback hc = new Handler.Callback() {
         public boolean handleMessage(Message msg) {
-
-
-
-            //public void handleMessage (android.os.Message msg) {
-                // обновляем TextView
-
+n = msg.what;
                 if (msg.what == STATUS_STOPPED) btnStart.setText("Стартовать Фотобот");
-                tvInfo.setText("Закачан файл: " + msg.what);
+              //  tvInfo.setText("Handler.Callback: Закачан файл: " + msg.what);
 
                 if (msg.what == 10) {
                     btnStart.setEnabled(true);
-                    tvInfo.setText("Закачано файлов: " + msg.what);
+//                    tvInfo.setText("Handler.Callback: Закачано файлов: " + msg.what);
 
                 }
+            if (msg.what == 4444) {
+                btnStart.setEnabled(true);
+                btnStop.setEnabled(false);
+//                    tvInfo.setText("Handler.Callback: Закачано файлов: " + msg.what);
 
-
-
-            //}
-
-
-
+            }
 
 
             Log.d(LOG_TAG, "what = " + msg.what);
@@ -55,9 +49,11 @@ public class MainActivity extends AppCompatActivity {
     final int STATUS_STARTED = 111;
     final int STATUS_WORKING = 222;
     final int STATUS_STOPPED = 333;
+    boolean STOP_FOTOBOT = false;
 
     final String LOG_TAG = "Logs";
     Button btnStart;
+    Button btnStop;
     Handler h;
     TextView tvInfo;
     TextView text;
@@ -75,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         intent = new Intent(MainActivity.this, Status.class);
         tvInfo.setText("Before handler starts");
 
-        h = new Handler(hc);
+
 
 
     }
@@ -91,17 +87,24 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onRestart() {
         super.onRestart();
+        tvInfo.setText("onRestart");
         Log.d(LOG_TAG, "onRestart");
     }
 
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        tvInfo.setText("onRestoreInstanceState");
         Log.d(LOG_TAG, "onRestoreInstanceState");
     }
 
     protected void onResume() {
         tvInfo = (TextView) findViewById(R.id.tvInfo);
         super.onResume();
+        tvInfo.setText("onResume" + n);
+
+        h = new Handler(hc);
+
+
         //tvInfo.setText("Закачано файлов: " + "returned from Status");
   /*      h = new Handler() {
             public void handleMessage(android.os.Message msg) {
@@ -125,16 +128,19 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        tvInfo.setText("onSaveInstanceState");
         Log.d(LOG_TAG, "onSaveInstanceState");
     }
 
     protected void onStart() {
         super.onStart();
+        tvInfo.setText("onStart");
         Log.d(LOG_TAG, "onStart");
     }
 
     protected void onStop() {
         super.onStop();
+        tvInfo.setText("onStop");
         Log.d(LOG_TAG, "onStop");
     }
 
@@ -181,10 +187,18 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button2:
                 btnStart.setEnabled(false);
                 btnStart.setText("Фотобот работает");
-
+                btnStop = (Button) findViewById(R.id.button2_1);
+                btnStop.setEnabled(true);
                 Thread t = new Thread(new Runnable() {
                     public void run() {
+
                         for (int i = 1; i <= 10; i++) {
+
+                            if (STOP_FOTOBOT) {
+                                h.sendEmptyMessage(4444);
+                                return;
+                            }
+
                             // долгий процесс
                             downloadFile();
 
@@ -195,21 +209,16 @@ public class MainActivity extends AppCompatActivity {
                             h.sendEmptyMessage(i);
 
 
+                            try {
+                                FileOutputStream fileout = openFileOutput("mytextfile.txt", MODE_PRIVATE | MODE_APPEND);
+                                OutputStreamWriter outputWriter = new OutputStreamWriter(fileout);
+                                outputWriter.write("Закачан файл:" + Integer.toString(i) + "\n");
+                                outputWriter.close();
 
 
-                                try {
-                                    FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_PRIVATE | MODE_APPEND);
-                                    OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
-                                    outputWriter.write("Закачан файл:" + Integer.toString(i) + "\n");
-                                    outputWriter.close();
-
-
-
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-
-
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
 
 
                             // пишем лог
@@ -221,14 +230,27 @@ public class MainActivity extends AppCompatActivity {
                 t.start();
 
                 break;
-           // case R.id.btnTest:
-           //     Log.d(LOG_TAG, "test");
-           //     break;
+            // case R.id.btnTest:
+            //     Log.d(LOG_TAG, "test");
+            //     break;
             default:
                 break;
         }
-
     }
+    public void stopFotobot(View v) {
+        STOP_FOTOBOT = true;
+
+        switch (v.getId()) {
+            case R.id.button2_1:
+                break;
+            // case R.id.btnTest:
+            //     Log.d(LOG_TAG, "test");
+            //     break;
+            default:
+                break;
+        }
+    }
+
     void downloadFile() {
         // пауза - 1 секунда
         try {
