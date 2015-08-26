1a3b3c6784835d52a4e8c9eea4b5aedab6711a4a
andreiyv <voran@inbox.ru>
2015-08-26 11:17:47 +0300

    From now MobileData depends on API version

--- app/src/main/java/com/example/andrey/fotobot/MobileData.java
+++ app/src/main/java/com/example/andrey/fotobot/MobileData.java
@@ -2,52 +2,68 @@ package com.example.andrey.fotobot;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.os.Build;
+import android.util.Log;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
  * Created by Andrey on 22.08.2015.
  */
 
+
 public class MobileData {
+    final String LOG_TAG = "Logs";
     public void setMobileDataEnabled(Context context, boolean enabled) {
-        try {
-           final ConnectivityManager conman = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            final Class conmanClass = Class.forName(conman.getClass().getName());
-            final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
-            iConnectivityManagerField.setAccessible(true);
-            final Object iConnectivityManager = iConnectivityManagerField.get(conman);
-            final Class iConnectivityManagerClass = Class.forName(iConnectivityManager.getClass().getName());
-            final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
-            setMobileDataEnabledMethod.setAccessible(true);
 
-            setMobileDataEnabledMethod.invoke(iConnectivityManager, enabled);
+        if (Build.VERSION.SDK_INT <= 10) {
+            Log.d(LOG_TAG, "Build.VERSION.SDK_INT" + Build.VERSION.SDK_INT);
+            try {
+                final ConnectivityManager conman = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                final Class conmanClass = Class.forName(conman.getClass().getName());
+                final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
+                iConnectivityManagerField.setAccessible(true);
+                final Object iConnectivityManager = iConnectivityManagerField.get(conman);
+                final Class iConnectivityManagerClass = Class.forName(iConnectivityManager.getClass().getName());
+                final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+                setMobileDataEnabledMethod.setAccessible(true);
 
+                setMobileDataEnabledMethod.invoke(iConnectivityManager, enabled);
 
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.d(LOG_TAG, "Build.VERSION.SDK_INT" + Build.VERSION.SDK_INT);
+                  ConnectivityManager dataManager;
+                        dataManager  = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                        Method dataMtd = null;
+                        try {
+                            dataMtd = ConnectivityManager.class.getDeclaredMethod("setMobileDataEnabled", boolean.class);
+                        } catch (NoSuchMethodException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                        dataMtd.setAccessible(true);
+                        try {
+                            dataMtd.invoke(dataManager, enabled);
+                        } catch (IllegalArgumentException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        } catch (IllegalAccessException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        } catch (InvocationTargetException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
 
-
-
-/*
-            final ConnectivityManager conman = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            final Class<?> conmanClass = Class.forName(conman.getClass().getName());
-            final Method setMobileDataEnabledMethod = conmanClass.getMethod("setMobileDataEnabled", Boolean.TYPE);
-            setMobileDataEnabledMethod.setAccessible(true);
-            setMobileDataEnabledMethod.invoke(conman, enabled);
-*/
-
-
-
-
-
-
-        }catch (Exception e) {
-            e.printStackTrace();
         }
     }
+
 }
-
-
 
 
 /*      ConnectivityManager dataManager;
@@ -74,26 +90,3 @@ public class MobileData {
         }
 */
 
-  /*      ConnectivityManager dataManager;
-                        dataManager  = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-                        Method dataMtd = null;
-                        try {
-                            dataMtd = ConnectivityManager.class.getDeclaredMethod("setMobileDataEnabled", boolean.class);
-                        } catch (NoSuchMethodException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                        dataMtd.setAccessible(true);
-                        try {
-                            dataMtd.invoke(dataManager, true);
-                        } catch (IllegalArgumentException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        } catch (IllegalAccessException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        } catch (InvocationTargetException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-*/
