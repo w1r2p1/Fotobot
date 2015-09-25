b1bf06fd7ee1540e5b1e459674fbac0f8f70e379
Andrey Voronin <andrey.voronin@cd-adapco.com>
2015-09-25 10:31:49 +0300

    Update

--- app/src/main/java/com/example/andrey/fotobot/Settings.java
+++ app/src/main/java/com/example/andrey/fotobot/Settings.java
@@ -1,59 +1,41 @@
 package com.example.andrey.fotobot;
 
 import android.app.TabActivity;
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.TabHost;
-import android.widget.TextView;
 
 public class Settings extends TabActivity {
 
-    private TabHost tabHost;
-
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-      tabHost = getTabHost();
-        TabHost.TabSpec spec;
-        Intent intent;
-        View view;
-        tabHost.getTabWidget().setDividerDrawable(R.drawable.divider);
-        view = createTabView(tabHost.getContext(), "Tab 1");
-        intent = new Intent().setClass(this, SomeClass1.class);
-        spec = tabHost.newTabSpec("tab1").setIndicator(view).setContent(intent);
-        tabHost.addTab(spec);
-  /*      intent = new Intent().setClass(this, SomeClass2.class);
-        view = createTabView(tabHost.getContext(), "Tab 2");
-        spec = tabHost.newTabSpec("tab2").setIndicator(view).setContent(intent);
-        tabHost.addTab(spec);
-        intent = new Intent().setClass(this, SomeClass3.class);
-        view = createTabView(tabHost.getContext(), "Tab 3");
-        spec = tabHost.newTabSpec("tab3").setIndicator(view).setContent(intent);
-        tabHost.addTab(spec);
-        */
+        // получаем TabHost
+        TabHost tabHost = getTabHost();
+
+        // инициализация была выполнена в getTabHost
+        // метод setup вызывать не нужно
+
+        TabHost.TabSpec tabSpec;
+
+        tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setIndicator("Вкладка 1");
+        tabSpec.setContent(new Intent(this, Tab_Main_Activity.class));
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator("Вкладка 2");
+        tabSpec.setContent(new Intent(this, Tab_Network_Activity.class));
+        tabHost.addTab(tabSpec);
+
+
     }
 
-
-
-    private static View createTabView(final Context context, final String text) {
-        View view = LayoutInflater.from(context).inflate(R.layout.tab_bg, null);
-        TextView tv = (TextView) view.findViewById(R.id.tabsText);
-        tv.setText(text);
-        return view;
-    }
 
 }
-
-
-
 
 
 
---
+++ app/src/main/java/com/example/andrey/fotobot/Tab_Network_Activity.java
@@ -0,0 +1,11 @@
+package com.example.andrey.fotobot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tab_Network_Activity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_network);
+    }
+}
