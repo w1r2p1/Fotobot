0b37220df65d2e90c7d051352d282d32df1f83c6
Andrey Voronin <andrey.voronin@cd-adapco.com>
2015-10-12 15:17:29 +0300

    Tabhost first commit

--- app/src/main/java/com/example/andrey/fotobot/Settings.java
+++ app/src/main/java/com/example/andrey/fotobot/Settings.java
@@ -1,19 +1,61 @@
 package com.example.andrey.fotobot;
 
 import android.app.TabActivity;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.CheckBox;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 public class Settings extends TabActivity {
 
+    private TabHost mTabHost;
+
+    private void setupTabHost() {
+        mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup();
+    }
 
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+// construct the tabhost
+        setContentView(R.layout.main);
+
+        setupTabHost();
+        mTabHost.getTabWidget().setDividerDrawable(R.drawable.tab_divider);
+
+        setupTab(new TextView(this), "Главная");
+        setupTab(new TextView(this), "Сеть");
+        setupTab(new TextView(this), "График");
+
+    }
+
+    private void setupTab(final View view, final String tag) {
+        View tabview = createTabView(mTabHost.getContext(), tag);
+
+        TabHost.TabSpec setContent = mTabHost.newTabSpec(tag).setIndicator(tabview).setContent(new TabHost.TabContentFactory() {
+            public View createTabContent(String tag) {return view;}
+        });
+        mTabHost.addTab(setContent);
+
+    }
+
+    private static View createTabView(final Context context, final String text) {
+        View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
+        TextView tv = (TextView) view.findViewById(R.id.tabsText);
+        tv.setText(text);
+        return view;
+    }
+
+
+
+
+/*
         setContentView(R.layout.main);
 
         // получаем TabHost
@@ -39,48 +81,6 @@ public class Settings extends TabActivity {
         tabSpec.setContent(new Intent(this, Tab_Scheduler_Activity.class));
         tabHost.addTab(tabSpec);
     }
-
-
-}
-
-
-
-
-/*public class Settings extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    public void showMain(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-}
 */
+
+}
