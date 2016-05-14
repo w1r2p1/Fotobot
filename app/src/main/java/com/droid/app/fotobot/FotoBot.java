b68df8bac7c95822e391496c50e80395cd9afb6b
Andrey <voran@inbox.ru>
2016-05-14 13:34:37 +0300

    update

--- app/src/main/java/com/droid/app/fotobot/FotoBot.java
+++ app/src/main/java/com/droid/app/fotobot/FotoBot.java
@@ -507,7 +507,7 @@ public class FotoBot extends Application {
 
         for (connect_attempt = 0; connect_attempt < 3; connect_attempt++) {
 
-            SendMessage(getResources().getString(R.string.connection_attempt) + (connect_attempt + 1));
+            SendMessage(getResources().getString(R.string.connection_attempt) + " " + (connect_attempt + 1));
 
             if (Network_Channel.contains("Wi-Fi")) {
                 SendMessage(getResources().getString(R.string.connection_channel_wifi));
