36e9e6d063ccfef4fc53e29d940d9b67db66fcf5
Andrey <voran@inbox.ru>
2016-08-19 21:48:21 +0300

    update

--- app/src/main/java/com/droid/app/fotobot/Tab_Main_Activity.java
+++ app/src/main/java/com/droid/app/fotobot/Tab_Main_Activity.java
@@ -880,8 +880,8 @@ public class Tab_Main_Activity extends Activity {
         FullFrame.addView(linLayout_SMS_Password);
       //  FullFrame.addView(linLayout_Storage);
         FullFrame.addView(linLayout_Work_Dir);
-        FullFrame.addView(linLayout_Delete_Foto);
         FullFrame.addView(linLayout1);
+        FullFrame.addView(linLayout_Delete_Foto);
         FullFrame.addView(linLayout_Adv_Settings_Log);
 
 
